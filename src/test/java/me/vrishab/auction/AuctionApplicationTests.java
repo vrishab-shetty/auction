8848e5a5bad43c2af6fc0114974be44fe3775842
vrishab <vrishabshetty@gmail.com>
2024-05-10 14:54:10 -0400

    Initial State of the Application

---
+++ src/test/java/me/vrishab/auction/AuctionApplicationTests.java
@@ -0,0 +1,13 @@
+package me.vrishab.auction;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuctionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
