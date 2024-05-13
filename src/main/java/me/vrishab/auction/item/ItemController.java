cb1ee4f3f350230459c4290860e4d8467b374550
vrishab <vrishabshetty@gmail.com>
2024-05-13 17:53:31 -0400

    Implement the Controller for Item
    	Implemented findItemById operation

---
+++ src/main/java/me/vrishab/auction/item/ItemController.java
@@ -0,0 +1,26 @@
+package me.vrishab.auction.item;
+
+import me.vrishab.auction.system.Result;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1")
+public class ItemController {
+
+    private final ItemService itemService;
+
+    @Autowired
+    public ItemController(ItemService itemService) {
+        this.itemService = itemService;
+    }
+
+    @GetMapping("/items/{itemId}")
+    public Result findItemById(@PathVariable String itemId) {
+        Item item = this.itemService.findById(itemId);
+        return new Result(true, "Find one Success", item);
+    }
+}
