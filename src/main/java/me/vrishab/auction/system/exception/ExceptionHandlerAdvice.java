3ca013d9b68281c0751cc14e034f4f04a6b70fce
vrishab <vrishabshetty@gmail.com>
2024-05-14 21:36:59 -0400

    Implement the API for finding user using Id
    	Implemented the findItemById operation

--- src/main/java/me/vrishab/auction/system/exception/ExceptionHandlerAdvice.java
+++ src/main/java/me/vrishab/auction/system/exception/ExceptionHandlerAdvice.java
@@ -2,13 +2,16 @@ package me.vrishab.auction.system.exception;
 
 import me.vrishab.auction.item.ItemNotFoundException;
 import me.vrishab.auction.system.Result;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
 public class ExceptionHandlerAdvice {
 
     @ExceptionHandler(ItemNotFoundException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
     public Result handleItemNotFoundException(ItemNotFoundException ex) {
         return new Result(false, ex.getMessage());
     }
--- src/test/java/me/vrishab/auction/item/ItemServiceTest.java
+++ src/test/java/me/vrishab/auction/item/ItemServiceTest.java
@@ -9,9 +9,7 @@ import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 
-import java.util.Optional;
-import java.util.Set;
-import java.util.UUID;
+import java.util.*;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.catchThrowable;
@@ -29,8 +27,47 @@ class ItemServiceTest {
     @InjectMocks
     private ItemService service;
 
+    private ArrayList<Item> items;
+
     @BeforeEach
     void setUp() {
+        items = new ArrayList<>();
+
+        Item item1 = new Item();
+        item1.setId(UUID.fromString("e2b2dd83-0e5d-4d73-b5cc-744f3fdc49a3"));
+        item1.setName("Nail");
+        item1.setDescription("A tool used for nailing two wooden piece");
+        item1.setLocation("NEU, Boston, MA");
+        item1.setImageUrls(Set.of("<images>"));
+        item1.setExtras(null);
+        item1.setLegitimacyProof("Proof");
+        item1.setAuctionId(UUID.fromString("c4838b19-9c96-45e0-abd7-c77d91af22b2"));
+        item1.setSeller("vr@domain.tld");
+        this.items.add(item1);
+
+        Item item2 = new Item();
+        item2.setId(UUID.fromString("e2b2dd83-0e5d-4d73-b5cc-744f3fdc49a2"));
+        item2.setName("Hammer");
+        item2.setDescription("A tool used for driving nails or breaking objects by striking.");
+        item2.setLocation("NEU, Boston, MA");
+        item2.setImageUrls(Set.of("<images>"));
+        item2.setExtras(null);
+        item2.setLegitimacyProof("Proof");
+        item2.setAuctionId(UUID.fromString("c4838b19-9c96-45e0-abd7-c77d91af22b2"));
+        item2.setSeller("vr@domain.tld");
+        this.items.add(item2);
+
+        Item item3 = new Item();
+        item3.setId(UUID.fromString("e2b2dd83-0e5d-4d73-b5cc-744f3fdc49a1"));
+        item3.setName("Computer");
+        item3.setDescription("Electronic device");
+        item3.setLocation("Amazon, San Fans, CA");
+        item3.setImageUrls(Set.of("<images>"));
+        item3.setExtras(null);
+        item3.setLegitimacyProof("Proof");
+        item3.setAuctionId(UUID.fromString("c4838b19-9c96-45e0-abd7-c77d91af22b1"));
+        item3.setSeller("vr@domain.tld");
+        this.items.add(item3);
     }
 
     @AfterEach
@@ -53,16 +90,7 @@ class ItemServiceTest {
             "auctionId": "c4838b19-9c96-45e0-abd7-c77d91af22b2",
             "seller": "vr@domain.tld"
         * */
-        Item item = new Item();
-        item.setId(UUID.fromString("e2b2dd83-0e5d-4d73-b5cc-744f3fdc49a3"));
-        item.setName("Hammer");
-        item.setDescription("A tool used for driving nails or breaking objects by striking.");
-        item.setLocation("NEU, Boston, MA");
-        item.setImageUrls(Set.of("<images>"));
-        item.setExtras(null);
-        item.setLegitimacyProof("Proof");
-        item.setAuctionId(UUID.fromString("c4838b19-9c96-45e0-abd7-c77d91af22b2"));
-        item.setSeller("vr@domain.tld");
+        Item item = items.get(0);
 
         given(repository.findById(UUID.fromString("e2b2dd83-0e5d-4d73-b5cc-744f3fdc49a3"))).willReturn(Optional.of(item));
 
@@ -98,5 +126,19 @@ class ItemServiceTest {
                 .isInstanceOf(ItemNotFoundException.class)
                 .hasMessage("Could not find item with Id e2b2dd83-0e5d-4d73-b5cc-744f3fdc49a3");
         verify(repository, times(1)).findById(UUID.fromString("e2b2dd83-0e5d-4d73-b5cc-744f3fdc49a3"));
+    }
+
+    @Test
+    void testFindAllSuccess() {
+
+        // Given
+        given(repository.findAll()).willReturn(items);
+
+        // When
+        List<Item> returnedItems = service.findAll();
+
+        // Then
+        assertThat(returnedItems.size()).isEqualTo(this.items.size());
+        verify(repository, times(1)).findAll();
     }
 }
