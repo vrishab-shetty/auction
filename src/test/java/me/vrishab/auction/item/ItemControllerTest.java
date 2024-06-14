7ffa1de1bbe65644ab14d2ff9b61906862bb6827
vrishab <vrishabshetty@gmail.com>
2024-06-13 21:08:18 -0400

    Fix: test case

--- src/test/java/me/vrishab/auction/item/ItemControllerTest.java
+++ src/test/java/me/vrishab/auction/item/ItemControllerTest.java
@@ -12,6 +12,7 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 
 import java.util.ArrayList;
@@ -25,6 +26,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @SpringBootTest
 @AutoConfigureMockMvc
+@ActiveProfiles("test")
 class ItemControllerTest {
 
     @MockBean
--- src/test/java/me/vrishab/auction/item/SpringDataJpaApplicationTests.java
+++ src/test/java/me/vrishab/auction/item/SpringDataJpaApplicationTests.java
@@ -1,14 +1,22 @@
 package me.vrishab.auction.item;
 
+import me.vrishab.auction.auction.Auction;
 import me.vrishab.auction.auction.AuctionRepository;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.TestInstance;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+import java.util.Calendar;
+import java.util.Date;
+import java.util.HashSet;
+import java.util.Set;
 
 @SpringBootTest
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
+@ActiveProfiles("test")
 abstract class SpringDataJpaApplicationTests {
 
     @Autowired
@@ -22,41 +30,41 @@ abstract class SpringDataJpaApplicationTests {
 
     @BeforeAll
     void setUp() {
-//        Auction auction = new Auction();
-//
-//        auction.setName("auction");
-//        Calendar calendar = Calendar.getInstance();
-//        calendar.setTime(new Date());
-//        calendar.add(Calendar.HOUR_OF_DAY, 1);
-//        auction.setStartTime(calendar.getTime().toInstant());
-//        calendar.add(Calendar.HOUR_OF_DAY, 1);
-//        auction.setEndTime(calendar.getTime().toInstant());
-//        auction.setInitialPrice(100.00);
-//        auction.setBuyer("vr@domain.tld");
-//        auction.setItems(generateItems());
-//
-//        auctionRepo.save(auction);
+        Auction auction = new Auction();
+
+        auction.setName("auction");
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime(new Date());
+        calendar.add(Calendar.HOUR_OF_DAY, 1);
+        auction.setStartTime(calendar.getTime().toInstant());
+        calendar.add(Calendar.HOUR_OF_DAY, 1);
+        auction.setEndTime(calendar.getTime().toInstant());
+        auction.setInitialPrice(100.00);
+        auction.setBuyer("vr@domain.tld");
+        auction.setItems(generateItems());
+
+        auctionRepo.save(auction);
     }
 
-//    private static Set<Item> generateItems() {
-//        Set<Item> items = new HashSet<>();
-//
-//        for (int i = 0; i < 10; i++) {
-//            Item item = new Item();
-//            item.setName("Item " + i + (i % 2 == 0 ? " (special)" : ""));
-//            item.setDescription("Description " + i);
-//            item.setLocation(i % 3 == 1 ? "MA" : "CA");
-//            item.setImageUrls(Set.of("<images>"));
-//            item.setExtras(null);
-//            item.setLegitimacyProof("Proof");
-//            item.setSeller("vr@domain.tld");
-//            items.add(item);
-//        }
-//
-////        items.forEach(System.out::println);
-//
-//        return items;
-//    }
+    private static Set<Item> generateItems() {
+        Set<Item> items = new HashSet<>();
+
+        for (int i = 0; i < 10; i++) {
+            Item item = new Item();
+            item.setName("Item " + i + (i % 2 == 0 ? " (special)" : ""));
+            item.setDescription("Description " + i);
+            item.setLocation(i % 3 == 1 ? "MA" : "CA");
+            item.setImageUrls(Set.of("<images>"));
+            item.setExtras(null);
+            item.setLegitimacyProof("Proof");
+            item.setSeller("vr@domain.tld");
+            items.add(item);
+        }
+
+//        items.forEach(System.out::println);
+
+        return items;
+    }
 
     @AfterAll
     void tearDown() {
